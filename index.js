1331b6c5c1437b68366fa7aca73a944ab8f00135
Adi-Codes-well <jhaa27211@gmail.com>
2025-07-12 01:50:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -64,6 +64,14 @@ const client = new Client({
   ],
 });
 
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => res.send("Bot is running!"));
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Web server running.");
+});
+
+
 client.once("ready", () => {
   console.log(`🤖 Logged in as ${client.user.tag}`);
 });
